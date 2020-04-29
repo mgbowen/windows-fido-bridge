b3332c33e27d14b8d60a7ac7590fe87d532a6a69
Matthew Bowen <matthew@mgbowen.com>
2020-04-29 19:45:04 -0400

    Add a custom fmt CMake script to make cross-compiling easy

---
+++ cmake/Findfmt.cmake
@@ -0,0 +1,24 @@
+find_path(
+    FMT_INCLUDE_DIR
+    fmt/format.h
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    fmt
+    REQUIRED_VARS FMT_INCLUDE_DIR
+)
+
+if (fmt_FOUND AND NOT TARGET fmt::fmt)
+    # Create a symlink in our build directory to avoid problems when
+    # cross-compiling with MinGW
+    set(_symlink_parent_dir "${CMAKE_BINARY_DIR}/fmt_shim")
+    file(MAKE_DIRECTORY "${_symlink_parent_dir}")
+    execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink "${FMT_INCLUDE_DIR}/fmt" "${_symlink_parent_dir}/fmt")
+    set(FMT_INCLUDE_DIR "${_symlink_parent_dir}")
+
+    add_library(fmt::fmt INTERFACE IMPORTED)
+    set_target_properties(fmt::fmt PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${FMT_INCLUDE_DIR}"
+    )
+endif()
--- src/common/include/windows_fido_bridge/format.hpp
+++ src/common/include/windows_fido_bridge/format.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+// Use fmt as a header-only library to make it easier to use when
+// cross-compiling with MinGW
 #define FMT_HEADER_ONLY
 #include <fmt/format.h>
 
